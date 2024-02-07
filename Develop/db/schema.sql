54aa8aa1ef8876eea4a60848f05a59b24c4e83de
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-07 15:01:47 -0600

    added the schema for creating a category and product table

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,18 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+-- CREATE TABLES
+CREATE TABLE Product (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(255),
+    price DECIMAL(10,2),
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+CREATE TABLE Category (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(255)
+);
