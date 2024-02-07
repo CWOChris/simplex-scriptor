a333412b7b65194e7229aaf20d688c2dbe4cf00b
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-07 16:06:38 -0600

    added the route to update a tag by ID value to tag-routes in routes/api dir

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -43,12 +43,26 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const [affectedRows] = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+    },
+  });
+    if (affectedRows > 0) {
+      res.status(200).json(affectedRows, { message: 'Tag ID updated successfully!' });
+    } else {
+      res.status(404).json({ message: 'Tag ID not found.' });
+    }} catch (err) {
+      res.status(500).json(err); // probably should be a 400 here
+    }
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  
 });
 
 module.exports = router;
