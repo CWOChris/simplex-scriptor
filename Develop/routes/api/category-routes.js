2d204f8155c60d1db8c27484268db2dcad55a22b
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-07 14:43:15 -0600

    adding express routes for get all categories and get single category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,15 +3,34 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {  // this is based on code in 11-Express activity 26 Heroku
   // find all categories
-  
-  // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      // be sure to include its associated Products
+      include: Product,  // this line may be incorrect
+    });
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: Product,
+    });
+    if (!category) {  
+      res.status(404).json({ message: 'Category ID not found.' });
+      return;
+    }
+    res.json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
